8ea24c1ffb478673063bd3b26c2611d8c71d4f0b
Angel <anzo3d@gmail.com>
2021-08-29 14:01:57 +0200

    Add Appbar login

---
+++ src/components/login/Appbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card tile>
+    <v-app-bar
+      color="#6A76AB"
+      dark
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-title>VUE APIREST</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Appbar",
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fill-height fluid px-0>
+    <v-row id="row-header">
+      <v-col id="col-header">
+        <appbar />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Appbar from "../components/login/Appbar.vue";
+
+export default {
+  name: "Login",
+
+  components: {
+    Appbar,
+  },
+};
+</script>
+
+<style>
+#row-header {
+  position: fixed;
+  /* background-color: white; */
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  top: 0;
+}
+
+#col-header {
+  padding: 0 !important;
+}
+</style>
